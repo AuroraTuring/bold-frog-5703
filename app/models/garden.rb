e016b90783535ee73b0e039398ef3b441b6f8b7c
Aurora Sanderson <AuroraTuring@gmail.com>
2024-06-10 15:37:56 -0600

    Build and establish relationships, create joins table

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,4 @@
 class Garden < ApplicationRecord
    has_many :plots
+   has_many :plot_plants
 end
---
+++ app/models/plot_plants.rb
@@ -0,0 +1,4 @@
+class GardenPlot < ApplicationRecord
+  belongs_to :garden
+  belongs_to :plot
+end
